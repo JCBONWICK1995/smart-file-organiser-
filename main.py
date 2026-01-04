5320d4232e09eb022440f103507da61ed07f8271
JCBONWICK1995 <jcbonwick1995@gmail.com>
2026-01-04 15:04:05 +0000

    Refactor organize_folder function for clarity and correctness
    Fix indentation, variable naming, and method calls in organize_folder function.

--- main.py
+++ main.py
@@ -11,42 +11,42 @@ EXTENTION_MAP = {
 }
 
 def organize_folder(target_path):
-#Ensure the path exists
-  if not os.path.exists(target_path):
-  print(f"Error: the path {target_path} does not exist.")
-  return
-
-#2. loo[ through every file in the folder
-for filename in os.listdir(target_path):
-
-  # Skip if it's a directory, we only want files
-
-if os.path.isdir(file_path):
-  continue
-
-#3. Identify the file extention
-_, extention = os.path.splitext(filename)
-extention = ectention.lower()
-
-#4 Find the right folder for this extention
-
-moved = False
-for folder_name, extention in EXTENTION_MAP.item():
-  if extention in extentions:
-    #create the catagory folder if it doesn't exist
-    dest_folder = os.path.joint(target_path, folder_name)
-    os.makedires(dest_folder, exist_ok=True)
-    # move the file
-    print(f"Moving {filename} -> {folder_name}/"
-          shutil.move(file_path, os.path.joint(dest_folder, filename))
-          moved = True
-          break
-
-# if the file type isn't in our map, put it in the 'Others'
-      if not moved:
-          others_folder = os.path.join(target_path, 'Others')
-          os.makedirs(others_folder, exist_ok=True)
-          shutil.move(file_path, os.path.join(others_folder, filename)
+  #Ensure the path exists
+    if not os.path.exists(target_path):
+    print(f"Error: the path {target_path} does not exist.")
+    return
+  
+  #2. loo[ through every file in the folder
+  for filename in os.listdir(target_path):
+  
+    # Skip if it's a directory, we only want files
+  
+  if os.path.isdir(file_path):
+    continue
+  
+  #3. Identify the file extention
+  _, extention = os.path.splitext(filename)
+  extention = ectention.lower()
+  
+  #4 Find the right folder for this extention
+  
+  moved = False
+  for folder_name, extention in EXTENTION_MAP.item():
+    if extention in extentions:
+      #create the catagory folder if it doesn't exist
+      dest_folder = os.path.joint(target_path, folder_name)
+      os.makedires(dest_folder, exist_ok=True)
+      # move the file
+      print(f"Moving {filename} -> {folder_name}/"
+            shutil.move(file_path, os.path.joint(dest_folder, filename))
+            moved = True
+            break
+  
+  # if the file type isn't in our map, put it in the 'Others'
+        if not moved:
+            others_folder = os.path.join(target_path, 'Others')
+            os.makedirs(others_folder, exist_ok=True)
+            shutil.move(file_path, os.path.join(others_folder, filename)
 
 if __name__ == "__main__":
                       # Test it on specific folder (Be careful! Use a test folder first)
